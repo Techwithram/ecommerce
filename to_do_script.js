0c2da6b963e87b517b69621583544d25b1a4c859
thatikonda ramakrishna <krishthatikonda1317@gmail.com>
2024-08-03 13:26:43 +0545

    to do web

---
+++ to_do_script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById("box");
+const listcontainer = document.getElementById("list-container");
+function addTask() {
+  if (input.value === "") {
+    alert("You must give a value");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  saveData();
+}
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showList() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showList();
